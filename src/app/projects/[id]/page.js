7ca35b1495556bbf67a54b60738d45970c5099bb
glima-dev <guilherme.lima.tec@live.com>
2024-05-06 03:03:12 -0300

    feat: create project dialog carousel

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -7,16 +7,57 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Icons } from "@/components/icons";
+import { Dialog, DialogClose, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 const Project = () => {
   const params = useParams();
   const currentProject = projectsList.find(
     (project) => params.id === project.endpoint
   );
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+
+  const handleOpenChange = () => {
+    setIsOpen((state) => !state);
+  };
+
+  const handleOpenImage = (imageIndex) => {
+    setCurrentImageIndex(imageIndex)
+    handleOpenChange()
+  }
+
+
 
   return (
     <>
+      <Dialog open={isOpen} onOpenChange={handleOpenChange} >
+        <DialogContent className="bg-transparent max-w-[820px] border-none">
+          <Carousel opts={{ startIndex: currentImageIndex }} className="mt-6 mx-5">
+            <CarouselContent className="bg-transparent border-none">
+              {currentProject.gallery.map((image, index) => (
+                <CarouselItem key={index} className="w-full">
+                  <Image
+                    src={image}
+                    alt={currentProject.title}
+                    width={633}
+                    height={554}
+                    className="w-full h-full"
+                  />
+
+                </CarouselItem>
+
+
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </DialogContent>
+      </Dialog>
+      <>
+
+      </>
       <main className="mt-14">
         <div className="flex justify-end container">
           <Button
@@ -44,7 +85,7 @@ const Project = () => {
 
           <ul className="w-full md:w-5/6 lg:w-1/2 flex flex-col gap-12 sm:gap-10 md:gap-16">
             {currentProject.gallery.map((image, index) => (
-              <li className="list-none cursor-zoom-in w-full md:w-full" key={index}>
+              <li className="list-none cursor-zoom-in w-full md:w-full" key={index} onClick={() => handleOpenImage(index)}>
                 <Image
                   src={image}
                   alt={currentProject.title}
--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -37,8 +37,8 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
       {...props}>
       {children}
       <DialogPrimitive.Close
-        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="h-4 w-4" />
+        className="absolute right-4 sm:-right-0 lg:-right-6 top-0 sm:-top-2 md:-top-3 lg:-top-6 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100  focus:outline-none focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+        <X className="h-8 w-8 text-white" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
