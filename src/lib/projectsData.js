311721e0572f8c09151a9977cb270664ce61b289
glima-dev <guilherme.lima.tec@live.com>
2024-07-05 01:21:22 -0300

    fotos projetos com imports e constantes

--- src/lib/projectsData.js
+++ src/lib/projectsData.js
@@ -7,6 +7,890 @@ const GalleryExample1 = "/gallery-example1.png";
 const GalleryExample2 = "/gallery-example2.png";
 const GalleryExample3 = "/gallery-example3.png";
 
+const Cgcs1 = "/cgcs_1.png";
+const Cgcs2 = "/cgcs_2.png";
+const Cgcs3 = "/cgcs_3.png";
+const Cgcs4 = "/cgcs_4.png";
+const Cgcs5 = "/cgcs_5.png";
+const Cgcs6 = "/cgcs_6.png";
+
+const Strk1 = "/strk_1.png";
+const Strk2 = "/strk_2.png";
+const Strk3 = "/strk_3.png";
+const Strk4 = "/strk_4.png";
+const Strk5 = "/strk_5.png";
+const Strk6 = "/strk_6.png";
+const Strk7 = "/strk_7.png";
+const Strk8 = "/strk_8.png";
+const Strk9 = "/strk_9.png";
+const Strk10 = "/strk_10.png";
+const Strk11 = "/strk_11.png";
+const Strk12 = "/strk_12.png";
+const Strk13 = "/strk_13.png";
+const Strk14 = "/strk_14.png";
+const Strk15 = "/strk_15.png";
+const Strk16 = "/strk_16.png";
+const Strk17 = "/strk_17.png";
+const Strk18 = "/strk_18.png";
+
+const Flc01 = "/flc_01.png";
+const Flc02 = "/flc_02.png";
+const Flc03 = "/flc_03.png";
+const Flc04 = "/flc_04.png";
+const Flc05 = "/flc_05.png";
+const Flc06 = "/flc_06.png";
+const Flc07 = "/flc_07.png";
+const Flc09 = "/flc_09.png";
+const Flc10 = "/flc_10.png";
+const Flc11 = "/flc_11.png";
+const Flc12 = "/flc_12.png";
+const Flc13 = "/flc_13.png";
+const Flc14 = "/flc_14.png";
+const Flc15 = "/flc_15.png";
+const Flc16 = "/flc_16.png";
+const Flc17 = "/flc_17.png";
+const Flc18 = "/flc_18.png";
+const Flc19 = "/flc_19.png";
+const Flc20 = "/flc_20.png";
+const Flc21 = "/flc_21.png";
+const Flc22 = "/flc_22.png";
+const Flc23 = "/flc_23.png";
+const Flc24 = "/flc_24.png";
+const Flc25 = "/flc_25.png";
+const Flc26 = "/flc_26.png";
+const Flc27 = "/flc_27.png";
+const Flc28 = "/flc_28.png";
+const Flc29 = "/flc_29.png";
+
+const Vrg1 = "/vrg_1.png";
+const Vrg2 = "/vrg_2.png";
+const Vrg3 = "/vrg_3.png";
+const Vrg4 = "/vrg_4.png";
+const Vrg5 = "/vrg_5.png";
+
+const Er1 = "/er_1.png";
+const Er2 = "/er_2.png";
+const Er3 = "/er_3.png";
+const Er4 = "/er_4.png";
+const Er5 = "/er_5.png";
+const Er6 = "/er_6.png";
+const Er7 = "/er_7.png";
+const Er8 = "/er_8.png";
+const Er9 = "/er_9.png";
+
+const Ln1 = "/ln_1.png";
+const Ln2 = "/ln_2.png";
+const Ln3 = "/ln_3.png";
+const Ln4 = "/ln_4.png";
+const Ln5 = "/ln_5.png";
+const Ln6 = "/ln_6.png";
+const Ln7 = "/ln_7.png";
+const Ln8 = "/ln_8.png";
+const Ln9 = "/ln_9.png";
+const Ln10 = "/ln_10.png";
+const Ln11 = "/ln_11.png";
+const Ln12 = "/ln_12.png";
+const Ln13 = "/ln_13.png";
+const Ln14 = "/ln_14.png";
+
+const Lr1 = "/lr_1.png";
+const Lr2 = "/lr_2.png";
+const Lr3 = "/lr_3.png";
+const Lr4 = "/lr_4.png";
+const Lr5 = "/lr_5.png";
+const Lr6 = "/lr_6.png";
+const Lr7 = "/lr_7.png";
+const Lr8 = "/lr_8.png";
+const Lr9 = "/lr_9.png";
+const Lr10 = "/lr_10.png";
+const Lr11 = "/lr_11.png";
+const Lr12 = "/lr_12.png";
+const Lr13 = "/lr_13.png";
+const Lr14 = "/lr_14.png";
+const Lr15 = "/lr_15.png";
+const Lr16 = "/lr_16.png";
+const Lr17 = "/lr_17.png";
+const Lr18 = "/lr_18.png";
+const Lr19 = "/lr_19.png";
+const Lr20 = "/lr_20.png";
+const Lr21 = "/lr_21.png";
+const Lr22 = "/lr_22.png";
+const Lr23 = "/lr_23.png";
+
+const Vv1 = "/vv_1.png";
+const Vv2 = "/vv_2.png";
+const Vv3 = "/vv_3.png";
+const Vv4 = "/vv_4.png";
+const Vv5 = "/vv_5.png";
+const Vv6 = "/vv_6.png";
+const Vv7 = "/vv_7.png";
+const Vv8 = "/vv_8.png";
+const Vv9 = "/vv_9.png";
+const Vv10 = "/vv_10.png";
+const Vv11 = "/vv_11.png";
+const Vv12 = "/vv_12.png";
+const Vv13 = "/vv_13.png";
+const Vv14 = "/vv_14.png";
+const Vv15 = "/vv_15.png";
+const Vv16 = "/vv_16.png";
+
+const Ch1 = "/ch_1.png";
+const Ch2 = "/ch_2.png";
+const Ch3 = "/ch_3.png";
+const Ch4 = "/ch_4.png";
+const Ch5 = "/ch_5.png";
+const Ch6 = "/ch_6.png";
+const Ch7 = "/ch_7.png";
+const Ch8 = "/ch_8.png";
+
+const Em1 = "/em_1.png";
+const Em2 = "/em_2.png";
+const Em3 = "/em_3.png";
+const Em4 = "/em_4.png";
+const Em5 = "/em_5.png";
+const Em6 = "/em_6.png";
+const Em7 = "/em_7.png";
+const Em8 = "/em_8.png";
+
+const Mm1 = "/mm_1.png";
+const Mm2 = "/mm_2.png";
+const Mm3 = "/mm_3.png";
+const Mm4 = "/mm_4.png";
+const Mm5 = "/mm_5.png";
+const Mm6 = "/mm_6.png";
+const Mm7 = "/mm_7.png";
+const Mm8 = "/mm_8.png";
+const Mm9 = "/mm_9.png";
+const Mm10 = "/mm_10.png";
+const Mm11 = "/mm_11.png";
+const Mm12 = "/mm_12.png";
+const Mm13 = "/mm_13.png";
+const Mm14 = "/mm_14.png";
+const Mm15 = "/mm_15.png";
+const Mm16 = "/mm_16.png";
+const Mm17 = "/mm_17.png";
+
+const Mdbv1 = "/mdbv_1.png";
+const Mdbv2 = "/mdbv_2.png";
+const Mdbv3 = "/mdbv_3.png";
+const Mdbv4 = "/mdbv_4.png";
+const Mdbv5 = "/mdbv_5.png";
+const Mdbv6 = "/mdbv_6.png";
+
+const In1 = "/in_1.png";
+const In2 = "/in_2.png";
+const In3 = "/in_3.png";
+const In4 = "/in_4.png";
+const In5 = "/in_5.png";
+const In6 = "/in_6.png";
+const In7 = "/in_7.png";
+const In8 = "/in_8.png";
+const In9 = "/in_9.png";
+const In10 = "/in_10.png";
+const In11 = "/in_11.png";
+
+const Sa1 = "/sa_1.png";
+const Sa2 = "/sa_2.png";
+const Sa3 = "/sa_3.png";
+const Sa4 = "/sa_4.png";
+const Sa5 = "/sa_5.png";
+const Sa6 = "/sa_6.png";
+const Sa7 = "/sa_7.png";
+const Sa8 = "/sa_8.png";
+const Sa9 = "/sa_9.png";
+const Sa10 = "/sa_10.png";
+const Sa11 = "/sa_11.png";
+const Sa12 = "/sa_12.png";
+const Sa13 = "/sa_13.png";
+const Sa14 = "/sa_14.png";
+const Sa15 = "/sa_15.png";
+const Sa16 = "/sa_16.png";
+const Sa17 = "/sa_17.png";
+const Sa18 = "/sa_18.png";
+const Sa19 = "/sa_19.png";
+const Sa20 = "/sa_20.png";
+const Sa21 = "/sa_21.png";
+const Sa22 = "/sa_22.png";
+const Sa23 = "/sa_23.png";
+const Sa24 = "/sa_24.png";
+
+const Cdvns1 = "/cdvns_1.png";
+const Cdvns2 = "/cdvns_2.png";
+const Cdvns3 = "/cdvns_3.png";
+const Cdvns4 = "/cdvns_4.png";
+const Cdvns5 = "/cdvns_5.png";
+const Cdvns6 = "/cdvns_6.png";
+const Cdvns7 = "/cdvns_7.png";
+const Cdvns8 = "/cdvns_8.png";
+const Cdvns9 = "/cdvns_9.png";
+const Cdvns10 = "/cdvns_10.png";
+const Cdvns11 = "/cdvns_11.png";
+const Cdvns12 = "/cdvns_12.png";
+const Cdvns13 = "/cdvns_13.png";
+const Cdvns14 = "/cdvns_14.png";
+const Cdvns15 = "/cdvns_15.png";
+const Cdvns16 = "/cdvns_16.png";
+const Cdvns17 = "/cdvns_17.png";
+const Cdvns18 = "/cdvns_18.png";
+const Cdvns19 = "/cdvns_19.png";
+const Cdvns20 = "/cdvns_20.png";
+const Cdvns21 = "/cdvns_21.png";
+const Cdvns22 = "/cdvns_22.png";
+const Cdvns23 = "/cdvns_23.png";
+const Cdvns24 = "/cdvns_24.png";
+const Cdvns25 = "/cdvns_25.png";
+const Cdvns26 = "/cdvns_26.png";
+const Cdvns27 = "/cdvns_27.png";
+const Cdvns28 = "/cdvns_28.png";
+const Cdvns29 = "/cdvns_29.png";
+const Cdvns30 = "/cdvns_30.png";
+const Cdvns31 = "/cdvns_31.png";
+const Cdvns32 = "/cdvns_32.png";
+const Cdvns33 = "/cdvns_33.png";
+
+const Sb01 = "/sb_01.png";
+const Sb02 = "/sb_02.png";
+const Sb03 = "/sb_03.png";
+const Sb04 = "/sb_04.png";
+const Sb05 = "/sb_05.png";
+const Sb06 = "/sb_06.png";
+const Sb07 = "/sb_07.png";
+const Sb08 = "/sb_08.png";
+const Sb09 = "/sb_09.png";
+const Sb10 = "/sb_10.png";
+const Sb11 = "/sb_11.png";
+const Sb12 = "/sb_12.png";
+const Sb13 = "/sb_13.png";
+const Sb14 = "/sb_14.png";
+const Sb15 = "/sb_15.png";
+const Sb16 = "/sb_16.png";
+const Sb17 = "/sb_17.png";
+const Sb18 = "/sb_18.png";
+const Sb19 = "/sb_19.png";
+const Sb20 = "/sb_20.png";
+const Sb21 = "/sb_21.png";
+const Sb22 = "/sb_22.png";
+const Sb23 = "/sb_23.png";
+
+const Ap1 = "/ap_1.png";
+const Ap2 = "/ap_2.png";
+const Ap3 = "/ap_3.png";
+const Ap4 = "/ap_4.png";
+const Ap5 = "/ap_5.png";
+const Ap6 = "/ap_6.png";
+const Ap7 = "/ap_7.png";
+const Ap8 = "/ap_8.png";
+const Ap9 = "/ap_9.png";
+const Ap10 = "/ap_10.png";
+const Ap11 = "/ap_11.png";
+const Ap12 = "/ap_12.png";
+const Ap13 = "/ap_13.png";
+const Ap14 = "/ap_14.png";
+const Ap15 = "/ap_15.png";
+const Ap16 = "/ap_16.png";
+const Ap17 = "/ap_17.png";
+const Ap18 = "/ap_18.png";
+const Ap19 = "/ap_19.png";
+const Ap20 = "/ap_20.png";
+const Ap21 = "/ap_21.png";
+const Ap22 = "/ap_22.png";
+
+const Md1 = "/md_1.png";
+const Md2 = "/md_2.png";
+const Md3 = "/md_3.png";
+const Md4 = "/md_4.png";
+const Md5 = "/md_5.png";
+const Md6 = "/md_6.png";
+const Md7 = "/md_7.png";
+const Md8 = "/md_8.png";
+const Md9 = "/md_9.png";
+const Md10 = "/md_10.png";
+const Md11 = "/md_11.png";
+const Md12 = "/md_12.png";
+const Md13 = "/md_13.png";
+const Md14 = "/md_14.png";
+const Md15 = "/md_15.png";
+const Md16 = "/md_16.png";
+const Md17 = "/md_17.png";
+const Md18 = "/md_18.png";
+const Md19 = "/md_19.png";
+const Md20 = "/md_20.png";
+const Md21 = "/md_21.png";
+const Md22 = "/md_22.png";
+const Md23 = "/md_23.png";
+const Md24 = "/md_24.png";
+
+const Rm1 = "/rm_1.png";
+const Rm2 = "/rm_2.png";
+const Rm3 = "/rm_3.png";
+const Rm4 = "/rm_4.png";
+const Rm5 = "/rm_5.png";
+const Rm6 = "/rm_6.png";
+const Rm7 = "/rm_7.png";
+const Rm8 = "/rm_8.png";
+const Rm9 = "/rm_9.png";
+const Rm10 = "/rm_10.png";
+const Rm11 = "/rm_11.png";
+const Rm12 = "/rm_12.png";
+const Rm13 = "/rm_13.png";
+const Rm14 = "/rm_14.png";
+const Rm15 = "/rm_15.png";
+
+const Tf1 = "/tf_1.png";
+const Tf2 = "/tf_2.png";
+const Tf3 = "/tf_3.png";
+const Tf4 = "/tf_4.png";
+const Tf5 = "/tf_5.png";
+const Tf6 = "/tf_6.png";
+const Tf7 = "/tf_7.png";
+const Tf8 = "/tf_8.png";
+const Tf9 = "/tf_9.png";
+const Tf10 = "/tf_10.png";
+const Tf11 = "/tf_11.png";
+const Tf12 = "/tf_12.png";
+const Tf13 = "/tf_13.png";
+const Tf14 = "/tf_14.png";
+const Tf15 = "/tf_15.png";
+const Tf16 = "/tf_16.png";
+
+const Lg01 = "/lg_01.png";
+const Lg02 = "/lg_02.png";
+const Lg03 = "/lg_03.png";
+const Lg04 = "/lg_04.png";
+const Lg0401 = "/lg_04_01.png";
+const Lg0402 = "/lg_04_02.png";
+const Lg0403 = "/lg_04_03.png";
+const Lg0501 = "/lg_05_01.png";
+const Lg06 = "/lg_06.png";
+const Lg07 = "/lg_07.png";
+const Lg08 = "/lg_08.png";
+const Lg0901 = "/lg_09_01.png";
+const Lg0902 = "/lg_09_02.png";
+const Lg0904 = "/lg_09_04.png";
+const Lg1001 = "/lg_10_01.png";
+const Lg1002 = "/lg_10_02.png";
+const Lg1003 = "/lg_10_03.png";
+const Lg1004 = "/lg_10_04.png";
+const Lg1005 = "/lg_10_05.png";
+const Lg11 = "/lg_11.png";
+const Lg12 = "/lg_12.png";
+const Lg13 = "/lg_13.png";
+const Lg14 = "/lg_14.png";
+const Lg15 = "/lg_15.png";
+const Lg16 = "/lg_16.png";
+const Lg17 = "/lg_17.png";
+
+const Nm01 = "/nm_01.png";
+const Nm02 = "/nm_02.png";
+const Nm03 = "/nm_03.png";
+const Nm04 = "/nm_04.png";
+const Nm05 = "/nm_05.png";
+const Nm06 = "/nm_06.png";
+const Nm07 = "/nm_07.png";
+const Nm08 = "/nm_08.png";
+const Nm09 = "/nm_09.png";
+const Nm10 = "/nm_10.png";
+const Nm11 = "/nm_11.png";
+const Nm12 = "/nm_12.png";
+const Nm13 = "/nm_13.png";
+const Nm14 = "/nm_14.png";
+const Nm15 = "/nm_15.png";
+const Nm16 = "/nm_16.png";
+const Nm17 = "/nm_17.png";
+const Nm18 = "/nm_18.png";
+const Nm19 = "/nm_19.png";
+const Nm20 = "/nm_20.png";
+const Nm21 = "/nm_21.png";
+
+const Pf1 = "/pf_1.png";
+const Pf2 = "/pf_2.png";
+const Pf3 = "/pf_3.png";
+const Pf4 = "/pf_4.png";
+const Pf5 = "/pf_5.png";
+const Pf6 = "/pf_6.png";
+const Pf7 = "/pf_7.png";
+const Pf8 = "/pf_8.png";
+const Pf9 = "/pf_9.png";
+const Pf10 = "/pf_10.png";
+const Pf11 = "/pf_11.png";
+const Pf12 = "/pf_12.png";
+const Pf13 = "/pf_13.png";
+const Pf14 = "/pf_14.png";
+const Pf15 = "/pf_15.png";
+
+const Lcs1 = "/lcs_1.png";
+const Lcs2 = "/lcs_2.png";
+const Lcs3 = "/lcs_3.png";
+const Lcs4 = "/lcs_4.png";
+const Lcs5 = "/lcs_5.png";
+const Lcs6 = "/lcs_6.png";
+const Lcs7 = "/lcs_7.png";
+const Lcs8 = "/lcs_8.png";
+const Lcs9 = "/lcs_9.png";
+const Lcs10 = "/lcs_10.png";
+const Lcs11 = "/lcs_11.png";
+const Lcs12 = "/lcs_12.png";
+const Lcs13 = "/lcs_13.png";
+const Lcs15 = "/lcs_15.png";
+const Lcs16 = "/lcs_16.png";
+const Lcs17 = "/lcs_17.png";
+const Lcs18 = "/lcs_18.png";
+const Lcs19 = "/lcs_19.png";
+const Lcs20 = "/lcs_20.png";
+const Lcs21 = "/lcs_21.png";
+const Lcs22 = "/lcs_22.png";
+const Lcs23 = "/lcs_23.png";
+const Lcs24 = "/lcs_24.png";
+const Lcs25 = "/lcs_25.png";
+const Lcs26 = "/lcs_26.png";
+const Lcs27 = "/lcs_27.png";
+const Lcs28 = "/lcs_28.png";
+const Lcs29 = "/lcs_29.png";
+const Lcs30 = "/lcs_30.png";
+const Lcs32 = "/lcs_32.png";
+
+const Cs1 = "/cs_1.png";
+const Cs2 = "/cs_2.png";
+const Cs3 = "/cs_3.png";
+const Cs4 = "/cs_4.png";
+const Cs5 = "/cs_5.png";
+const Cs6 = "/cs_6.png";
+const Cs7 = "/cs_7.png";
+const Cs8 = "/cs_8.png";
+const Cs9 = "/cs_9.png";
+
+const cgcsImages = [Cgcs1, Cgcs2, Cgcs3, Cgcs4, Cgcs5, Cgcs6];
+
+const strkImages = [
+    Strk1,
+    Strk2,
+    Strk3,
+    Strk4,
+    Strk5,
+    Strk6,
+    Strk7,
+    Strk8,
+    Strk9,
+    Strk10,
+    Strk11,
+    Strk12,
+    Strk13,
+    Strk14,
+    Strk15,
+    Strk16,
+    Strk17,
+    Strk18,
+];
+
+const flcImages = [
+    Flc01,
+    Flc02,
+    Flc03,
+    Flc04,
+    Flc05,
+    Flc06,
+    Flc07,
+    Flc09,
+    Flc10,
+    Flc11,
+    Flc12,
+    Flc13,
+    Flc14,
+    Flc15,
+    Flc16,
+    Flc17,
+    Flc18,
+    Flc19,
+    Flc20,
+    Flc21,
+    Flc22,
+    Flc23,
+    Flc24,
+    Flc25,
+    Flc26,
+    Flc27,
+    Flc28,
+    Flc29,
+];
+
+const vrgImages = [Vrg1, Vrg2, Vrg3, Vrg4, Vrg5];
+
+const erImages = [Er1, Er2, Er3, Er4, Er5, Er6, Er7, Er8, Er9];
+
+const lnImages = [
+    Ln1,
+    Ln2,
+    Ln3,
+    Ln4,
+    Ln5,
+    Ln6,
+    Ln7,
+    Ln8,
+    Ln9,
+    Ln10,
+    Ln11,
+    Ln12,
+    Ln13,
+    Ln14,
+];
+
+const lrImages = [
+    Lr1,
+    Lr2,
+    Lr3,
+    Lr4,
+    Lr5,
+    Lr6,
+    Lr7,
+    Lr8,
+    Lr9,
+    Lr10,
+    Lr11,
+    Lr12,
+    Lr13,
+    Lr14,
+    Lr15,
+    Lr16,
+    Lr17,
+    Lr18,
+    Lr19,
+    Lr20,
+    Lr21,
+    Lr22,
+    Lr23,
+];
+
+const vvImages = [
+    Vv1,
+    Vv2,
+    Vv3,
+    Vv4,
+    Vv5,
+    Vv6,
+    Vv7,
+    Vv8,
+    Vv9,
+    Vv10,
+    Vv11,
+    Vv12,
+    Vv13,
+    Vv14,
+    Vv15,
+    Vv16,
+];
+
+const chImages = [Ch1, Ch2, Ch3, Ch4, Ch5, Ch6, Ch7, Ch8];
+
+const emImages = [Em1, Em2, Em3, Em4, Em5, Em6, Em7, Em8];
+
+const mmImages = [
+    Mm1,
+    Mm2,
+    Mm3,
+    Mm4,
+    Mm5,
+    Mm6,
+    Mm7,
+    Mm8,
+    Mm9,
+    Mm10,
+    Mm11,
+    Mm12,
+    Mm13,
+    Mm14,
+    Mm15,
+    Mm16,
+    Mm17,
+];
+
+const mdbvImages = [Mdbv1, Mdbv2, Mdbv3, Mdbv4, Mdbv5, Mdbv6];
+
+const inImages = [In1, In2, In3, In4, In5, In6, In7, In8, In9, In10, In11];
+
+const saImages = [
+    Sa1,
+    Sa2,
+    Sa3,
+    Sa4,
+    Sa5,
+    Sa6,
+    Sa7,
+    Sa8,
+    Sa9,
+    Sa10,
+    Sa11,
+    Sa12,
+    Sa13,
+    Sa14,
+    Sa15,
+    Sa16,
+    Sa17,
+    Sa18,
+    Sa19,
+    Sa20,
+    Sa21,
+    Sa22,
+    Sa23,
+    Sa24,
+];
+
+const cdvnsImages = [
+    Cdvns1,
+    Cdvns2,
+    Cdvns3,
+    Cdvns4,
+    Cdvns5,
+    Cdvns6,
+    Cdvns7,
+    Cdvns8,
+    Cdvns9,
+    Cdvns10,
+    Cdvns11,
+    Cdvns12,
+    Cdvns13,
+    Cdvns14,
+    Cdvns15,
+    Cdvns16,
+    Cdvns17,
+    Cdvns18,
+    Cdvns19,
+    Cdvns20,
+    Cdvns21,
+    Cdvns22,
+    Cdvns23,
+    Cdvns24,
+    Cdvns25,
+    Cdvns26,
+    Cdvns27,
+    Cdvns28,
+    Cdvns29,
+    Cdvns30,
+    Cdvns31,
+    Cdvns32,
+    Cdvns33,
+];
+
+const sbImages = [
+    Sb01,
+    Sb02,
+    Sb03,
+    Sb04,
+    Sb05,
+    Sb06,
+    Sb07,
+    Sb08,
+    Sb09,
+    Sb10,
+    Sb11,
+    Sb12,
+    Sb13,
+    Sb14,
+    Sb15,
+    Sb16,
+    Sb17,
+    Sb18,
+    Sb19,
+    Sb20,
+    Sb21,
+    Sb22,
+    Sb23,
+];
+
+const apImages = [
+    Ap1,
+    Ap2,
+    Ap3,
+    Ap4,
+    Ap5,
+    Ap6,
+    Ap7,
+    Ap8,
+    Ap9,
+    Ap10,
+    Ap11,
+    Ap12,
+    Ap13,
+    Ap14,
+    Ap15,
+    Ap16,
+    Ap17,
+    Ap18,
+    Ap19,
+    Ap20,
+    Ap21,
+    Ap22,
+];
+
+const mdImages = [
+    Md1,
+    Md2,
+    Md3,
+    Md4,
+    Md5,
+    Md6,
+    Md7,
+    Md8,
+    Md9,
+    Md10,
+    Md11,
+    Md12,
+    Md13,
+    Md14,
+    Md15,
+    Md16,
+    Md17,
+    Md18,
+    Md19,
+    Md20,
+    Md21,
+    Md22,
+    Md23,
+    Md24,
+];
+
+const rmImages = [
+    Rm1,
+    Rm2,
+    Rm3,
+    Rm4,
+    Rm5,
+    Rm6,
+    Rm7,
+    Rm8,
+    Rm9,
+    Rm10,
+    Rm11,
+    Rm12,
+    Rm13,
+    Rm14,
+    Rm15,
+];
+
+const tfImages = [
+    Tf1,
+    Tf2,
+    Tf3,
+    Tf4,
+    Tf5,
+    Tf6,
+    Tf7,
+    Tf8,
+    Tf9,
+    Tf10,
+    Tf11,
+    Tf12,
+    Tf13,
+    Tf14,
+    Tf15,
+    Tf16,
+];
+
+const lgImages = [
+    Lg01,
+    Lg02,
+    Lg03,
+    Lg04,
+    Lg0401,
+    Lg0402,
+    Lg0403,
+    Lg0501,
+    Lg06,
+    Lg07,
+    Lg08,
+    Lg0901,
+    Lg0902,
+    Lg0904,
+    Lg1001,
+    Lg1002,
+    Lg1003,
+    Lg1004,
+    Lg1005,
+    Lg11,
+    Lg12,
+    Lg13,
+    Lg14,
+    Lg15,
+    Lg16,
+    Lg17,
+];
+
+const nmImages = [
+    Nm01,
+    Nm02,
+    Nm03,
+    Nm04,
+    Nm05,
+    Nm06,
+    Nm07,
+    Nm08,
+    Nm09,
+    Nm10,
+    Nm11,
+    Nm12,
+    Nm13,
+    Nm14,
+    Nm15,
+    Nm16,
+    Nm17,
+    Nm18,
+    Nm19,
+    Nm20,
+    Nm21,
+];
+
+const pfImages = [
+    Pf1,
+    Pf2,
+    Pf3,
+    Pf4,
+    Pf5,
+    Pf6,
+    Pf7,
+    Pf8,
+    Pf9,
+    Pf10,
+    Pf11,
+    Pf12,
+    Pf13,
+    Pf14,
+    Pf15,
+];
+
+const lcsImages = [
+    Lcs1,
+    Lcs2,
+    Lcs3,
+    Lcs4,
+    Lcs5,
+    Lcs6,
+    Lcs7,
+    Lcs8,
+    Lcs9,
+    Lcs10,
+    Lcs11,
+    Lcs12,
+    Lcs13,
+    Lcs15,
+    Lcs16,
+    Lcs17,
+    Lcs18,
+    Lcs19,
+    Lcs20,
+    Lcs21,
+    Lcs22,
+    Lcs23,
+    Lcs24,
+    Lcs25,
+    Lcs26,
+    Lcs27,
+    Lcs28,
+    Lcs29,
+    Lcs30,
+    Lcs32,
+];
+
+const csImages = [Cs1, Cs2, Cs3, Cs4, Cs5, Cs6, Cs7, Cs8, Cs9];
+
 const projectsList = [
     {
         title: "Projeto 1",
@@ -15,7 +899,20 @@ const projectsList = [
             "Este projeto arquitetônico visa harmonizar funcionalidade e estética, garantindo espaços fluidos e inspiradores. Combinando elementos contemporâneos e sustentáveis, nossa equipe busca criar ambientes que transcendam expectativas, oferecendo soluções inovadoras e adaptáveis às necessidades dos clientes. Nosso compromisso é com a excelência, garantindo que cada projeto seja entregue dentro do prazo, do orçamento e com a plena satisfação do cliente. Trabalhando em estreita colaboração com nossos clientes, buscamos proporcionar experiências únicas e duradouras através da arquitetura.",
         emphasisContent: true,
         coverImage: Cover1,
-        gallery: [GalleryExample1, GalleryExample2, GalleryExample3, GalleryExample1, GalleryExample2, GalleryExample3, GalleryExample1, GalleryExample2, GalleryExample3, GalleryExample1, GalleryExample2, GalleryExample3],
+        gallery: [
+            GalleryExample1,
+            GalleryExample2,
+            GalleryExample3,
+            GalleryExample1,
+            GalleryExample2,
+            GalleryExample3,
+            GalleryExample1,
+            GalleryExample2,
+            GalleryExample3,
+            GalleryExample1,
+            GalleryExample2,
+            GalleryExample3,
+        ],
         category: "business",
         locationType: "INTERIOR",
     },
